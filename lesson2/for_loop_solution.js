8c55f94cdd9537a04d56ef45bd3d0ec9871f181c
rmikhailenko <mikhoilenko.roman@gmail.com>
2018-03-15 01:07:45 +0200

    loops

---
+++ lesson2/for_loop_solution.js
@@ -0,0 +1,21 @@
+function numberGenerator() {
+  var min = 1, max = 20, rand;
+  rand = Math.floor(min + Math.random() * (max + 1 - min));
+  var result;
+  for (var i = 0; i < 5; i++) {
+    if (rand % 2 == 0) {
+      result = rand + ' четное';
+    } else {
+      result = rand + ' нечетное';
+    }
+  }
+  return result;
+}
+
+console.log(numberGenerator());
+
+function multiplicationTable() {
+}
+
+function checkAssignGrade() {
+}
