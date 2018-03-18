88a209abc4c78b58d7c5229322a274d965af61e6
Sergey Petrunin <neyasut@gmail.com>
2018-03-18 21:48:24 +0200

    loop

--- lesson2/for_loop_solution.js
+++ lesson2/for_loop_solution.js
@@ -8,17 +8,25 @@ function numberGenerator() {
     } else {
       result = rand + ' нечетное';
     }
+    console.log(result);
   }
-  console.log(result);
-  console.log(result);
-  console.log(result);
-  console.log(result);
-  console.log(result);
 }
-  //numberGenerator();
+  numberGenerator();
 
 function multiplicationTable() {
+for (var i = 1; i <=10; i++) {
+  for (var j = 1; j <= 10; j++) {
+    console.log(i + ' * ' + j + ' = ' + i*j );
+  }
+  }
 }
+multiplicationTable();
 
 function checkAssignGrade() {
+  var result;
+  for (var i = 80; i <= 100; i++) {
+    console.log(assignGrade(i));
+  }
+  return result;
 }
+console.log(checkAssignGrade());
