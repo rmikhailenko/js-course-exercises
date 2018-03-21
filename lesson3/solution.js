e1acb7c72a429fd9f9fed041c3c45c58159dea20
rmikhailenko <you@example.com>
2018-03-21 18:33:14 +0200

    loop

--- lesson3/solution.js
+++ lesson3/solution.js
@@ -12,9 +12,19 @@ return false;
 }
 hasRepeatingElements([1,2,3,4,5,6]);
 
-function negativeElementsSum() {
 
+var arr = [1,2,0,-1,2,-3];
+function negativeElementsSum(arr) {
+var n = arr.length;
+var result;
+  for (var i = 1; i < n; i++) {
+    if (arr[i] < 0) {
+      console.log(arr[i]);
+    }
+  }
+  return result;
 }
+negativeElementsSum([1,-2,3,-4,5,-6]);
 
 function commonElements() {
 }
